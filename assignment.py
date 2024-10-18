074721e24c96a3b1089ef3af0999988fe626d51c
Zubia-Syed <Zubia.syed@stu.ocsb.ca>
2024-10-18 14:48:58 +0000

    python changes

--- assignment.py
+++ assignment.py
@@ -1,1 +1,11 @@
-
+diameter=(input)
+if diameter <= 0: 
+  print("reenter diameter number must be greater than 0")
+elif diameter > 0:
+  def calc(d):
+    r=d/2
+    print(r)
+    area= math.pi*r**2
+    circumference=2*math.pi*2
+    print(circumference)
+    print(area)
