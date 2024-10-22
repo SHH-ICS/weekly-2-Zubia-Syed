a7bcc01f0cee14652e07849b3027b6942be9c929
Zubia-Syed <Zubia.syed@stu.ocsb.ca>
2024-10-22 21:48:22 +0000

    Changes 3

--- handler.php
+++ handler.php
@@ -1,44 +1,50 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <title>Welcome Page</title>
-  </head>
 
-  <body>
-    
-    <?php
-    $myVariable = "diameter";
-    if ( isset( $_POST['diameter'] ) ){
-      $diameter = $_POST['diameter'];
-    }
+<head>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <title>Welcome Page</title>
+</head>
 
-if ($diameter <= 0) {
+<body>
 
-echo "Reenter diameter: number must be greater than 0.";
-} else {
-}
-function calc($d) {
+  <?php
+  $myVariable = "diameter";
+  $diameter = 0;
+  $circumference = '';
+  $area = '';
+  if (isset($_POST['diameter'])) {
+    $diameter = $_POST['diameter'];
+  }
 
-    $r=$d/2;
-    echo($r);
-    $area= pi()*$r**2;
+  if ($diameter <= 0) {
+
+    echo "Reenter diameter: number must be greater than 0.";
+  } else {
+  }
+  function calc($d)
+  {
+
+    $r = $d / 2;
+    echo ($r);
+    $area = pi() * $r ** 2;
     $circumference = 2 * pi() * $r;
+    return [$area, $circumference];
 
 
-    echo($area);
-    echo($circumference);
-}
+    echo ($area);
+    echo ($circumference);
+  }
 
-    calc($diameter);
+  calc($diameter);
 
-    echo "<h1>My Program</h1>\n";
-    echo "<p>$circumference= = ".$circumference."</p>\n";
-    echo "<p>$area= = " . $area. "</p>\n";
-    ?>
-    
-  </body>
-  
-  </html>
+  echo "<h1></h1>\n";
+  echo "<p>Circumference: " . $circumference . "</p>\n";
+  echo "<p>Area: " . $area . "</p>\n";
+  ?>
+
+</body>
+
+</html>
